58e34a9a78dee0f1233a4d39c671a432886bf437
mattlynley <you@example.com>
2016-06-02 16:15:16 -0700

    Grabbing TM headlines. Sorry Gabe.

--- newsgrabber.py
+++ newsgrabber.py
@@ -5,18 +5,48 @@ import re
 
 ## This will basically extract any novel information or scoops from a story.
 
-url = raw_input("Input a URL:\n>")
+def getTechmemeUrls():
 
-def getScoopz(url):
+	techmeme = urllib2.urlopen("http://techmeme.com/")
+	read = techmeme.read()
+	soup = BeautifulSoup(read, "html.parser")
 
-	urlopen = urllib2.urlopen(url)
+	heds = soup.find_all(class_="L4")
+	# heds2 = soup.find_all(class_="L2")
+	hedstr = ""
+	
+	for i in heds:
+		hedstr += str(i)
+
+	url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', hedstr)
+
+	return(url)
+
+	# for i in heds:
+	# 	hedstr += str(i) + "\n"
+	# for i in heds2:
+	# 	hedstr += str(i) + "\n"
+
+	# # hedstr = re.sub(r'<[^>]*>', '', hedstr)
+
+	# return(hedstr)
+
+def getScoopz():
+
+	url = raw_input("Input a URL:\n>")
+
+	try:	
+		urlopen = urllib2.urlopen(url)
+	except:
+		return("Can't open the site :(")
+
 	read = urlopen.read()
 	soup = BeautifulSoup(read, "html.parser")
 
 	paras = soup.find_all('p')
 	headline = soup.find_all(class_="lede-headline__highlighted")
 
-	phrases = ['hearing', 'source', 'said the people', 'familiar', 'person', 'matter']
+	phrases = ['hearing', 'source', 'familiar', 'person', 'matter']
 
 	array = str(paras).split("<p>")
 
@@ -51,8 +81,13 @@ def getScoopz(url):
 
 	newstr = ""
 
-	for i in newarray:
-		newstr += i + ". "
+	i = 0
+
+	## Get rid of an annoying last period
+
+	while i < len(newarray) - 1:
+		newstr += newarray[i] + ". "
+		i += 1
 
 	## Remove codes
 
@@ -62,9 +97,12 @@ def getScoopz(url):
 
 	newstr = re.sub(r'<[^>]*>', '', newstr)
 
-	return(newstr)
+	if newstr != "":
+		return(newstr)
+	else:
+		return("There's no news!")
 
-print(getScoopz(url))
+print(getTechmemeUrls())
 
 # stringremove2 = stringremove1.replace("</p>", "")
 # stringremove3 = stringremove2.replace("\u2019", "'")
