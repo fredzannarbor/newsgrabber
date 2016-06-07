6db01f320fa9bee7e51f974b6ab9fa95cb0bfffd
mattlynley <you@example.com>
2016-06-07 13:29:54 -0700

    bare bones

--- application.py
+++ application.py
@@ -10,17 +10,9 @@ application = Flask(__name__)
 
 ## This will basically extract any novel information or scoops from a story.
 @application.route("/", methods=['GET', 'POST'])
-def home(object=None):
-	
-	tmUrls = getTechmemeUrls.TechmemeUrls()
-	
-	scoop = getscoopz.GetScoopz()
-
-	urls = tmUrls.getTechmemeUrls()
-
-	scoops = scoop.getScoopz(urls)
-
-	return render_template('hello.html')
+def home(URL=None):
+    
+    return render_template('hello.html')
 
 if __name__ == '__main__':
 	application.run()
