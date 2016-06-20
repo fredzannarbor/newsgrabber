1df076044b6acced7e1ced690b7c1e3a35d6a96e
mattlynley <you@example.com>
2016-06-20 14:37:15 -0700

    figuring out sql

--- sqltesting.py
+++ sqltesting.py
@@ -6,23 +6,15 @@ cursor = connection.cursor()
 
 namecheck = raw_input("Enter a name\n>").upper()
 
-cursor.execute("SELECT * FROM commonnames WHERE NAME LIKE 'A%'")
+cursor.execute("SELECT * FROM commonnames")
 
-
-fetcharray = []
 result = cursor.fetchall()
+for i in result:
+	print i
 
 for i in result:
-	name = str(i[0])
-	counter = int(i[1])
-	fetcharray.append([name.upper(), counter])
-
-print fetcharray
-
-
-for i in fetcharray:
-	if i[0] == namecheck:
-		cursor.execute("UPDATE commonnames SET COUNT=COUNT+1 WHERE NAME=?",(i[0]))
+	if str(i[0]) == namecheck:
+	 	cursor.execute("UPDATE commonnames SET COUNT=COUNT+1 WHERE NAME=?",(i[0],))
 	
 connection.commit()
 
